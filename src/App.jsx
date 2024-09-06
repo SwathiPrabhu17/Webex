21fdf1c0c3da48d7117552caa5c9007f33e85b0c
ms-poojary <varshinipoojary04@gmail.com>
2024-09-06 17:18:43 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <>
+     
+      <h1>Vite + React</h1>
+      <Navbar/>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Navbar.css'
+
+export default function navbar() {
+  return (
+    <div>
+        <h1 className='navbar'>hello vacchu how are you</h1>
+    </div>
+  )
+}
